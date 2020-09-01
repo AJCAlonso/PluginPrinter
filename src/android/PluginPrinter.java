3d53023455b56836dd3b9fbaa71ea16e5cb5834b
Adriano Alonso <adrianojcalonso@gmail.com>
2020-08-31 22:58:22 -0300

    cordova

--- src/android/PluginPrinter.java
+++ src/android/PluginPrinter.java
@@ -2,6 +2,8 @@ package cordova.plugin.saurusprinter;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
